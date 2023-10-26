00236edbaf6a367bd3a1f636f9d42eb6d1b1d7b7
anapauor <62265692+anapauor@users.noreply.github.com>
2023-10-26 19:26:26 -0300

    Update gpt_program.py

--- gpt_program.py
+++ gpt_program.py
@@ -7,18 +7,6 @@ load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 openai.api_key = api_key
 
-# modelo = "text-davinci-002"
-# prompt = "¿qué día es hoy?"
-
-# respuesta = openai.Completion.create(
-#     engine=modelo,
-#     prompt=prompt,
-#     n=1,
-#     max_tokens = 5
-# ) 
-
-# print(respuesta)
-
 def create_content(topic, tokens, temperature, model="text-davinci-002"):
     prompt = f"Please write an abstract abount {topic} \n\n"
     respuesta = openai.Completion.create(
