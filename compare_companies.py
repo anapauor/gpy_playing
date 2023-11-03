024edcb638c02de04b019bb94f2f9deeeff13e70
anapauor <anapaula.orri@gmail.com>
2023-11-03 16:29:42 -0300

    polishing compare companies

--- compare_companies.py
+++ compare_companies.py
@@ -1,6 +1,7 @@
 
 
 import os
+from posixpath import split
 import openai
 import textwrap
 import yfinance as yf
@@ -11,33 +12,46 @@ load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 openai.api_key = api_key
 
+# Select financials and take querterly income and Get yfinance data
+
+def select_stocks():
+    """
+    Function for creating a list of stocks from an input string separeted by commas
+    """
+    stocks = input("Select stock: ")
+    stocks_list = stocks.split(",")
+    return stocks_list
+
+
 def get_quarterly_income(company_name):
-    company = yf.Ticker(company_name)
+    """
+    Function for getting quarterly income table report for company
+    """
+    print(company_name)
+    company = yf.Ticker(company_name.strip())
     return company.quarterly_income_stmt
 
-def compare_companies(first_company, second_company):
+
+def compare_companies(companies):
     """
-    Function for comparing two companies performance in last quarter period
+    Function for compailing each companie info in a dict
     """
-    first_company_income = get_quarterly_income(first_company)
-    second_company_income = get_quarterly_income(second_company)
+    companies_dict = {}
+    for c in companies:  
+        companies_dict[c.strip()] = get_quarterly_income(c)
 
-    return first_company_income, second_company_income
+    return companies_dict
 
-    return first_stock_quarterly_income, seccond_stock_quarterly_income
 
-# Get yfinance data
-stock = input("Select stock: ").upper()
+companies = select_stocks()
+companies_dict = compare_companies(companies)
 
-# select financials and take querterly income:
-selected_stock = yf.Ticker(stock)
-stock_quarterly_income = (selected_stock.quarterly_income_stmt)
-msft_stock = selected_stock = yf.Ticker("MSFT")
-msft_quarterly_income = (msft_stock.quarterly_income_stmt)
+
 
 # Create OpenAI promt
-prompt = f"Based on the following two companies financial data, which company performed better? Firts company: {stock_quarterly_income} Seccond company: {msft_quarterly_income}"
+prompt = f"Based on the following cosmpanies financial data, which company performed better and why?: {companies_dict}"
 
+print(prompt)
 
 # Configure OpenAI response
 response = openai.Completion.create(
